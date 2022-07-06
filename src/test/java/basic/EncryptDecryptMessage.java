3ce40b370b0c69dd904a44a96f254850d60d1ae7
william walsh <william.walsh@digitary.net>
2022-07-06 16:55:05 +0100

    Remove results folder from git tracking. Add encryption and decryption flows using private and public key.

--- src/test/java/basic/EncryptDecryptMessage.java
+++ src/test/java/basic/EncryptDecryptMessage.java
@@ -5,14 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.crypto.Cipher;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.*;
 import java.security.cert.X509Certificate;
+import java.util.stream.Stream;
 
 public class EncryptDecryptMessage {
 
+    public static final String KEY_ALIAS = "doc_signer";
+    public static final String ENCRYPTED_FILE = "results/encrypted_string.bin";
     protected KeyStore ks;
 
     public void initKeyStore(String keystore, String ks_pass)
@@ -36,8 +42,7 @@ public class EncryptDecryptMessage {
     public byte[] encrypt(Key key, String message) throws GeneralSecurityException {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] cipherData = cipher.doFinal(message.getBytes());
-        return cipherData;
+        return cipher.doFinal(message.getBytes());
     }
 
     public String decrypt(Key key, byte[] message) throws GeneralSecurityException {
@@ -53,10 +58,56 @@ public class EncryptDecryptMessage {
     }
 
     @Test
-    public void encryptString() throws GeneralSecurityException {
-        String message = "Top Secret";
-        Key publicKey = getPublicKey("doc_signer");
+    public void encryptThenDecryptString() throws GeneralSecurityException, IOException {
+        String message = "H1dd3n";
+        Key publicKey = getPublicKey(KEY_ALIAS);
+        Key privateKey = getPrivateKey(KEY_ALIAS, SignDocumentTest.KEY_STORE_PASSWORD);
+
+        // Encrypt with pub, decrypt with pri
         byte[] encryptedMessage = encrypt(publicKey, message);
-        System.out.println(String.format("%X", new BigInteger(1, encryptedMessage)));
+        String unencryptedMessage = decrypt(privateKey, encryptedMessage);
+        System.out.println(unencryptedMessage);
+
+        // Encrypt with pri, decrypt with pub
+        encryptedMessage = encrypt(privateKey, message);
+        unencryptedMessage = decrypt(publicKey, encryptedMessage);
+        System.out.println(unencryptedMessage);
+    }
+
+    @Test
+    public void encryptThenDecryptFileContent() throws GeneralSecurityException, IOException {
+        String message = "H1dd3n";
+        Key publicKey = getPublicKey(KEY_ALIAS);
+        Key privateKey = getPrivateKey(KEY_ALIAS, SignDocumentTest.KEY_STORE_PASSWORD);
+
+        // Encrypt with public key
+        byte[] encryptedMessage = encrypt(publicKey, message);
+
+        // write byte[] to file
+        try (FileOutputStream fos = new FileOutputStream(ENCRYPTED_FILE)) {
+            fos.write(encryptedMessage);
+        }
+
+        // read byte[] from file
+        File file = new File(ENCRYPTED_FILE);
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+
+        // decrypt byte[]
+        String unencryptedMessage = decrypt(privateKey, fileContent);
+
+        // print original message
+        System.out.println(unencryptedMessage);
+    }
+
+    @Test
+    public void decryptStringFromFileUsingPrivateKey() throws GeneralSecurityException, IOException {
+        String inputFile = "results/encrypted_string.bin";
+        Key privateKey = getPrivateKey(KEY_ALIAS, SignDocumentTest.KEY_STORE_PASSWORD);
+        Path encryptedFilePath = Paths.get("results/encrypted_string.bin");
+
+        byte[] fileBytes = Files.readAllBytes(encryptedFilePath);
+
+        String decryptedMessage = decrypt(privateKey, fileBytes);
+        System.out.println("Decrypted Message: " + decryptedMessage);
     }
 }
--- src/test/java/basic/ListSecurityProviders.java
+++ src/test/java/basic/ListSecurityProviders.java
@@ -1,2 +1,17 @@
-package basic;public class ListSecurityProviders {
+package basic;
+
+import org.junit.Test;
+
+import java.security.*;
+
+public class ListSecurityProviders {
+
+    @Test
+    public void printProvidersAndCiphers() {
+        for (Provider provider: Security.getProviders()) {
+            System.out.println(provider.getName());
+            for (String key: provider.stringPropertyNames())
+                System.out.println("\t" + key + "\t" + provider.getProperty(key));
+        }
+    }
 }
